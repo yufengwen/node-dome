9223e45f6daa1d8f50efc06c855118e245b7c58c
xwg <xwg@lindeMacBook-Pro.local>
2016-05-10 15:21:43 +0800

    图片压缩

--- static/gulpfile.js
+++ static/gulpfile.js
@@ -1,30 +1,37 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var minify = require('gulp-minify-css');
+var imagemin = require('gulp-imagemin');
+
 var path = {
     js: 'js/*.js',
-    css: 'css/*.css'
+    css: 'css/*.css',
+    img: 'img/*.*'
 }
-gulp.task('script', function(){
+gulp.task('script', () => {
     gulp.src(path.js)
         .pipe(uglify())
         .pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('css', function(){
+gulp.task('css', () => {
     gulp.src(path.css)
         .pipe(minify())
         .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('watch-js', function(){
-    gulp.watch(path.js, ['script']);
+gulp.task('img', () => {
+    gulp.src(path.img)
+        .pipe(imagemin({
+            progressive: true
+        }))
+        .pipe(gulp.dest('dist/images'));
 });
 
-gulp.task('watch-css', function(){
-    gulp.watch(path.css, ['css']);
+gulp.task('watch', () => {
+    gulp.watch([path.js, path.css, path.img], ['script', 'css', 'img'])
 });
 
-gulp.task('pack', ['watch-js', 'watch-css', 'script', 'css']);
+gulp.task('pack', ['script', 'css', 'img', 'watch']);
 
 gulp.task('default', ['pack']);
